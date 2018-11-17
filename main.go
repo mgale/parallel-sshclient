3723e9cf0b71d89bb82490bc3da7e530746168e4
Michael Gale <gale.michael@gmail.com>
2018-11-17 10:24:47 -0700

    Add max concurrent code

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net"
-	"os"
 	"os/user"
 	"strings"
+	"sync"
 	"time"
 
 	"golang.org/x/crypto/ssh"
@@ -20,6 +20,12 @@ type sshData struct {
 	username   string
 	port       int
 	privatekey string
+}
+
+type sshClient struct {
+	sshData
+	timeout    int32
+	runCommand string
 }
 
 func loadHostFile(hostnamesfile *string, sshHostInfo map[string]sshData) {
@@ -34,13 +40,15 @@ func loadHostFile(hostnamesfile *string, sshHostInfo map[string]sshData) {
 		}
 		connectString := strings.Split(line, "@")
 		myhostname := ""
+		myusername := ""
 		if len(connectString) == 1 {
 			myhostname = connectString[0]
 		} else {
+			myusername = connectString[0]
 			myhostname = connectString[1]
 		}
 
-		sshHostInfo[myhostname] = sshData{myhostname, "", 22, ""}
+		sshHostInfo[myhostname] = sshData{myhostname, myusername, 22, ""}
 	}
 }
 
@@ -58,83 +66,104 @@ func PublicKeyFile(file string) ssh.AuthMethod {
 	return ssh.PublicKeys(key)
 }
 
-func runCommand(mysshdata sshData, ch chan<- string) {
+//saveResults dumps command output to file in /tmp/
+func saveResults(hostname string, output []byte) bool {
+	timestamp := time.Now().Unix()
 
+	err := ioutil.WriteFile(fmt.Sprintf("/tmp/%s-%d.log", hostname, timestamp), output, 0644)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func runCommand(sshclient sshClient) (string, bool) {
+
+	myStatus := false
 	start := time.Now()
 	sshConfig := &ssh.ClientConfig{
-		User: mysshdata.username,
+		User: sshclient.username,
 		Auth: []ssh.AuthMethod{
-			PublicKeyFile(mysshdata.privatekey),
+			PublicKeyFile(sshclient.privatekey),
 		},
 		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
 			return nil
 		},
+		Timeout: time.Duration(rand.Int31n(sshclient.timeout)) * time.Millisecond,
 	}
 
-	connection, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", mysshdata.hostname, mysshdata.port), sshConfig)
+	connection, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", sshclient.hostname, sshclient.port), sshConfig)
 	if err != nil {
-		ch <- fmt.Sprint(err)
-		return
+		return fmt.Sprintf("%s : %s", sshclient.hostname, err), myStatus
 	}
 
 	session, err := connection.NewSession()
 	if err != nil {
-		ch <- fmt.Sprint(err)
-		return
+		return fmt.Sprintf("%s : %s", sshclient.hostname, err), myStatus
 	}
 
-	modes := ssh.TerminalModes{
-		ssh.ECHO:          0,     // disable echoing
-		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
-		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	out, err := session.CombinedOutput(sshclient.runCommand)
+	session.Close()
+	connection.Close()
+
+	if err == nil {
+		myStatus = true
 	}
 
-	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
-		session.Close()
-		ch <- fmt.Sprint(err)
-		return
+	saveStatus := saveResults(sshclient.hostname, out)
+	return fmt.Sprintf("%.2fs elapsed Host: %s : CommandSuccess: %t : output-saved: %t\n", time.Since(start).Seconds(), sshclient.hostname, myStatus, saveStatus), myStatus
+}
+
+func printUpdates(ch <-chan string, chShutDown <-chan bool) {
+
+	chShutDownClosed := false
+
+	for {
+		if chShutDownClosed {
+			return
+		}
+		select {
+		case logUpdate, logUpdateOk := <-ch:
+			if logUpdateOk {
+				log.Printf(logUpdate)
+			}
+		case myChannelData, myChannelOpen := <-chShutDown:
+			chShutDownClosed = true
+			if myChannelData {
+				log.Println("Completed all hosts")
+			}
+			if !myChannelOpen {
+				log.Println("Channel closed: No more updates")
+			}
+		}
 	}
 
-	stdin, err := session.StdinPipe()
-	if err != nil {
-		ch <- fmt.Sprint(err)
-		return
+}
+
+func worker(ch chan<- string, chStatus chan<- bool, chsshClient <-chan sshClient, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for sshclient := range chsshClient {
+		cmdLog, cmdStatus := runCommand(sshclient)
+		chStatus <- cmdStatus
+		ch <- cmdLog
 	}
-	go io.Copy(stdin, os.Stdin)
-
-	stdout, err := session.StdoutPipe()
-	if err != nil {
-		ch <- fmt.Sprint(err)
-		return
-	}
-	go io.Copy(os.Stdout, stdout)
-
-	stderr, err := session.StderrPipe()
-	if err != nil {
-		ch <- fmt.Sprint(err)
-		return
-	}
-	go io.Copy(os.Stderr, stderr)
-
-	err = session.Run("echo $HOSTNAME")
-
-	ch <- fmt.Sprintf("%.2fs %s elapsed\n", time.Since(start).Seconds(), mysshdata.hostname)
 }
 
 func main() {
 
+	wg := new(sync.WaitGroup)
 	start := time.Now()
+	sshHostInfo := make(map[string]sshData)
+
 	myUser, err := user.Current()
 	if err != nil {
 		log.Println("Error determining current username")
 		log.Fatal(err)
 	}
 
-	sshHostInfo := make(map[string]sshData)
-	ch := make(chan string)
-
 	hostnamesfile := flag.String("hosts-file", "", "File containing hostnames")
-	concurCount := flag.Int("concurrent", 50, "Number of parallel connections to make")
+	conCurMax := flag.Int("curmax", 50, "Number of parallel connections to make")
 	remoteCommand := flag.String("remote-cmd", "echo $HOSTNAME", "Number of parallel connections to make")
 	sshUser := flag.String("l", myUser.Username, "SSH Username")
 	sshKey := flag.String("i", fmt.Sprintf("%s/.ssh/id_rsa", myUser.HomeDir), "Private key file to use")
@@ -150,22 +179,49 @@ func main() {
 	log.Printf("Loading hostnames file: %s", *hostnamesfile)
 	loadHostFile(hostnamesfile, sshHostInfo)
 	log.Printf("Loaded %d hosts", len(sshHostInfo))
-	log.Printf("Concurrency count: %d", *concurCount)
+	log.Printf("Concurrency count: %d", *conCurMax)
 	log.Printf("Remote Command: %s", *remoteCommand)
+
+	ch := make(chan string)
+	chShutDown := make(chan bool)
+	chStatus := make(chan bool, len(sshHostInfo))
+	chsshClient := make(chan sshClient, len(sshHostInfo))
+
+	go printUpdates(ch, chShutDown)
 
 	for _, mysshData := range sshHostInfo {
 		if mysshData.username == "" {
 			mysshData.username = *sshUser
 			mysshData.privatekey = *sshKey
-			mysshData.port = 22
 		}
 
-		go runCommand(mysshData, ch)
+		chsshClient <- sshClient{mysshData, 5000, *remoteCommand}
 	}
 
-	for range sshHostInfo {
-		log.Printf(<-ch)
+	close(chsshClient)
+
+	for i := 0; i < *conCurMax; i++ {
+		wg.Add(1)
+		go worker(ch, chStatus, chsshClient, wg)
 	}
 
+	//Wait for all work goroutines to finish
+	wg.Wait()
+	//Shutdown channel, stopping the printUpdates goroutine
+	chShutDown <- true
+
+	successResults := 0
+	failedResults := 0
+	close(chStatus)
+	for result := range chStatus {
+		if result {
+			successResults++
+		} else {
+			failedResults++
+		}
+	}
+
+	failedHosts := len(sshHostInfo) - successResults - failedResults
+	log.Printf("Total Hosts: %d    Commands Succesful: %d    Commands Failed:  %d    Hosts Failed: %d", len(sshHostInfo), successResults, failedResults, failedHosts)
 	log.Printf("%.2fs Overall elapsed time\n", time.Since(start).Seconds())
 }
